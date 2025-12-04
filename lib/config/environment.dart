fe7287a4d7399b58be5224e1774502e07f548325
Pola <your-email@example.com>
2025-12-04 18:21:11 +0100

    Your commit message here

--- lib/config/environment.dart
+++ lib/config/environment.dart
@@ -1,12 +1,5 @@
 class Environment {
-  // These can be overridden at build time using
-  // --dart-define=APPWRITE_PROJECT_ID=... and --dart-define=APPWRITE_ENDPOINT=...
-  static const String appwriteProjectId =
-      String.fromEnvironment('APPWRITE_PROJECT_ID', defaultValue: '690641ad0029b51eefe0');
-
+  static const String appwriteProjectId = '690641ad0029b51eefe0';
   static const String appwriteProjectName = 'XapZap';
-
-  static const String appwritePublicEndpoint =
-      String.fromEnvironment('APPWRITE_ENDPOINT', defaultValue: 'https://nyc.cloud.appwrite.io/v1');
+  static const String appwritePublicEndpoint = 'https://nyc.cloud.appwrite.io/v1';
 }
-
